68ded0d06105cf003ecbb16a499425ef862d55e3
ahmedhosnimjar <ahmedmjar7@gmail.com>
2026-06-08 11:34:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-function answer(correct) {
+function check(correct) {
     const result = document.getElementById("result");
 
     if (correct) {
         result.innerText = "Correct!";
-        result.style.color = "lightgreen";
+        result.style.color = "green";
     } else {
-        result.innerText = "Wrong answer";
+        result.innerText = "Wrong!";
         result.style.color = "red";
     }
+}
+
+function toggleNight() {
+    document.body.classList.toggle("night");
+}
+
+function toggleReading() {
+    document.body.classList.toggle("reading");
 }
